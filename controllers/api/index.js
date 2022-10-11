3d1e082fe18441e2e0e74749fd0443a550c72d5d
meriem0319 <meriem0319@gmail.com>
2022-10-11 17:52:56 -0400

    readme with screenshots, heroku not working still

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,8 @@ const userRoutes = require("./user-routes.js");
 const postRoutes = require("./post-routes.js");
 const commentRoutes = require("./comment-routes.js");
 
-router.use("/users", userRoutes);
-router.use("/posts", postRoutes);
-router.use("/comments", commentRoutes);
+router.use("/user", userRoutes);
+router.use("/post", postRoutes);
+router.use("/comment", commentRoutes);
 
 module.exports = router;
